7776d64341d958207dc86a27d08a24ee26f12b74
xiepeidong <xiepeidong@sogou-inc.com>
2016-03-03 11:03:27 +0800

    modify JSONUtil.

--- commons/src/main/java/com/sogou/pay/common/utils/JSONUtil.java
+++ commons/src/main/java/com/sogou/pay/common/utils/JSONUtil.java
@@ -9,28 +9,24 @@ import java.util.Map;
  * Created by hujunfei Date: 14-12-30 Time: 下午1:37
  */
 public class JSONUtil {
-
-    public static PMap jsonToPMap(String value, PMap map) {
-        return JSON.parseObject(value, PMap.class);
-    }
-
-    public static Map JSON2Map(String json){
+    public static Map JSON2Map(String json) {
         return JSON.parseObject(json, Map.class);
     }
-    public static PMap JSON2PMap(String json){
+
+    public static PMap JSON2PMap(String json) {
         return JSON.parseObject(json, PMap.class);
     }
 
-    public static String Map2JSON(Map map){
+    public static String Map2JSON(Map map) {
         return JSON.toJSONString(map);
     }
 
-    public static <T> T JSON2Bean(String json, Class<T> clazz){
+    public static <T> T JSON2Bean(String json, Class<T> clazz) {
         return JSON.parseObject(json, clazz);
     }
 
 
-    public static String Bean2JSON(Object bean){
+    public static String Bean2JSON(Object bean) {
         return JSON.toJSONString(bean);
     }
 }
