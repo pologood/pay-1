26a01b998315c5b126c9482906ec21e1ccc5288b
kinghost <kingraser@gmail.com>
2016-06-15 12:08:33 +0800

    unionpay initially created

---
+++ thirdpay-service/src/main/java/com/sogou/pay/thirdpay/service/Unionpay/UnionpayService.java
@@ -0,0 +1,186 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2015 Sogou.com. All Rights Reserved.
+ */
+package com.sogou.pay.thirdpay.service.Unionpay;
+
+import com.sogou.pay.common.exception.ServiceException;
+import com.sogou.pay.common.types.PMap;
+import com.sogou.pay.common.types.ResultMap;
+import com.sogou.pay.thirdpay.service.ThirdpayService;
+
+//--------------------- Change Logs----------------------
+//@author wangwenlong Initial Created at 2016年6月15日;
+//-------------------------------------------------------
+public class UnionpayService implements ThirdpayService {
+
+  @Override
+  public ResultMap preparePayInfoAccount(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap preparePayInfoGatway(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap preparePayInfoQRCode(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap preparePayInfoSDK(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap preparePayInfoWap(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap queryOrder(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap refundOrder(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap queryRefundOrder(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap downloadOrder(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap prepareTransferInfo(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifyWebSync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifyWebAsync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifyWapSync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifyWapAsync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifySDKAsync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifyRefund(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap getReqIDFromNotifyTransfer(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifyWebSync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifyWebAsync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifyWapSync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifyWapAsync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifySDKAsync(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifyRefund(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+  @Override
+  public ResultMap handleNotifyTransfer(PMap params) throws ServiceException {
+    // TODO Auto-generated method stub
+    return null;
+
+  }
+
+}
