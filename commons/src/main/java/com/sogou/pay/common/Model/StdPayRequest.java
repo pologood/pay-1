16579736748b4410ecb404c2e3013c0adaa40243
kinghost <kingraser@gmail.com>
2016-07-01 10:48:20 +0800

    fix possible null pointer exception

--- commons/src/main/java/com/sogou/pay/common/Model/StdPayRequest.java
+++ commons/src/main/java/com/sogou/pay/common/Model/StdPayRequest.java
@@ -245,7 +245,7 @@ public class StdPayRequest {
   }
 
   public String getOrderAmountString() {
-    return orderAmount.toString();
+    return orderAmount == null ? null : orderAmount.toString();
   }
 
 }
