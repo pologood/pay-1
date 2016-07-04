3009999f6a0bb3c6756bd606a50019ee4b017090
kinghost <kingraser@gmail.com>
2016-07-04 11:46:06 +0800

    重构全工程签名

--- commons/src/main/java/com/sogou/pay/common/utils/SignUtil.java
+++ commons/src/main/java/com/sogou/pay/common/utils/SignUtil.java
@@ -1,236 +1,121 @@
 package com.sogou.pay.common.utils;
 
-import com.google.common.collect.Maps;
-
-import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.security.MessageDigest;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Objects;
 
 /**
  * Created by hujunfei Date: 15-1-4 Time: 下午2:22
  */
 public class SignUtil {
 
-    public static final String DEFAULT_SIG_NAME = "sign";
-    private static final String ALGORITHM = "RSA";
-    private static final String SIGN_ALGORITHMS = "SHA1WithRSA";
-    private static final String DEFAULT_CHARSET = "UTF-8";
+  private static final Logger LOGGER = LoggerFactory.getLogger(SignUtil.class);
 
-    private static Logger logger = LoggerFactory.getLogger(SignUtil.class);
+  private static final String RSA = "RSA", SHA1WITHRSA = "SHA1WithRSA", CHARSET = "UTF-8";
 
-    //使用ThreadLocal变量，防止线程冲突
-    private static ThreadLocal<MessageDigest> mdThreadLocal = new ThreadLocal<>();
-
-    // TODO: 增加其他加密方法
-    public static MessageDigest getMD() {
-        MessageDigest md = mdThreadLocal.get();
-        if (md == null) {
-            try {
-                md = MessageDigest.getInstance("MD5");
-                mdThreadLocal.set(md);
-            } catch (Exception e) {
-                logger.error("Get MessageDigest Error!", e);
-            }
-        }
-        return md;
+  public static String md5Hex(String origin, String charset) {
+    try {
+      return DigestUtils.md5Hex(origin.getBytes(charset));
+    } catch (Exception e) {
+      LOGGER.error("[md5Hex]error", e);
+      return null;
     }
+  }
 
-    /**
-     * 校验签名是否正确
-     *
-     * @param params  签名参数
-     * @param secret  密钥
-     * @return 签名是否正确
-     */
-    public static boolean checkSignMD5(String sign, Map params, String secret, String charset) {
-        if (params == null || params.size() == 0) {
-            throw new IllegalArgumentException("params is null or empty");
-        }
-        if (StringUtils.isBlank(sign)) {
-            throw new IllegalArgumentException("sign is blank");
-        }
-        if (StringUtils.isBlank(secret)) {
-            throw new IllegalArgumentException("secret is blank");
-        }
+  public static String md5Hex(String origin) {
+    return md5Hex(origin, CHARSET);
+  }
 
-        String calculateSig = signMD5(params, secret, charset);
-        // System.out.println("Compute: "+calculateSig+", Origin:"+sig);
-
-        if (sign.trim().equals(calculateSig)) {
-            return true;
-        }
-        logger.warn("Check Sig error Params: " + params + ",Compute: " + calculateSig + ",Origin:" + sign);
-        return false;
+  public static String shaHex(String origin, String charset) {
+    try {
+      return DigestUtils.shaHex(origin.getBytes(charset));
+    } catch (Exception e) {
+      LOGGER.error("[shaHex]error", e);
+      return null;
     }
+  }
 
-    /**
-     * 计算签名，sigName排除在外，null键和null值也排除在外。不修改原始params
-     *
-     * @param params  签名参数
-     * @param secret  密钥
-     * @return 签名结果
-     */
-    public static String signMD5(Map params, String secret, String charset) {
-        if (StringUtils.isBlank(secret)) {
-            throw new IllegalArgumentException("secret key is null or empty");
-        }
-        return encodeMD5(packParams(params, secret), charset);
+  public static String shaHex(String origin) {
+    return shaHex(origin, CHARSET);
+  }
+
+  public static String loadKeyFromFile(String keyFilePath) {
+    try {
+      return StringUtils.join(Files.readAllLines(Paths.get(keyFilePath)), null);
+    } catch (Exception e) {
+      LOGGER.error(String.format("[loadKeyFromFile]path=%s", keyFilePath), e);
+      return null;
     }
+  }
 
-    @Deprecated
-    public static String calculateSig(String sigName, Map params, String secret, String charset) {
-        if (StringUtils.isBlank(secret)) {
-            throw new IllegalArgumentException("secret key is null or empty");
-        }
-        return encodeMD5(packParams(params, secret), charset);
+  public static String signSHA1WithRSA(String text, String privateKey, String charset) {
+    try {
+      Signature signature = Signature.getInstance(SHA1WITHRSA);
+
+      signature.initSign(
+          KeyFactory.getInstance(RSA).generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey))));
+      signature.update(text.getBytes(charset));
+
+      return Base64.getEncoder().encodeToString(signature.sign());
+    } catch (Exception e) {
+      LOGGER.error("[signSHA1WithRSA]error", e);
+      return null;
     }
+  }
 
-    private static String packParams(Map paramMap, String secret) {
-        if (paramMap == null) {
-            throw new IllegalArgumentException("params is null or empty");
-        }
-        Map newMap = MapUtil.dropNulls(Maps.newHashMap(paramMap));
-        if (MapUtil.isEmpty(newMap)) {
-            throw new IllegalArgumentException("params is null or empty");
-        }
-        List<String> keyList = new ArrayList<String>(paramMap.keySet());
-        Collections.sort(keyList);
+  public static String signSHA1WithRSA(String text, String privateKey) {
+    return signSHA1WithRSA(text, privateKey, CHARSET);
+  }
 
-        //拼接k1=v1k2=v2
-        StringBuilder paramStrBuilder = new StringBuilder();
-        for (int i = 0; i < keyList.size(); i++) {
-            String key = keyList.get(i);
-            Object value = paramMap.get(key);
+  private static boolean checkRSASign(String content, String sign, String publicKey, String charset) {
+    try {
+      Signature signature = Signature.getInstance(SHA1WITHRSA);
 
-            if (!StringUtils.isEmpty((String)value)) {
-                paramStrBuilder.append(key).append("=").append(value.toString());
-                if (i != keyList.size() - 1) {//拼接时，不包括最后一个&字符
-                    paramStrBuilder.append("&");
-                }
-            }
-        }
+      signature.initVerify(
+          KeyFactory.getInstance(RSA).generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey))));
+      signature.update(content.getBytes(charset));
 
-        //拼接secretKey
-        paramStrBuilder.append(secret);
-        return paramStrBuilder.toString();
+      return signature.verify(Base64.getDecoder().decode(sign));
+    } catch (Exception e) {
+      LOGGER.error("[checkRSASign]error", e);
+      return false;
     }
+  }
 
-    public static void main(String[] args) {
-        System.out.println(encodeMD5("accessPlatform=3&appId=5000&bankId=ALIPAY&bgUrl=http://10.134.76.150:8899??op=mszy_pay_feedback&orderAmount=1.00&orderId=20151103113505850805&orderTime=20151103113505&pageUrl=http://www.sogou.com&productDesc=中医支付测试&productName=中医测试&productNum=1&signType=0&version=V1.0idf059023sdkgfo03482fd", "utf-8"));
-    }
-    private static String encodeMD5(String encodeParam, String charset) {
-        if (encodeParam == null) {
-            return null;
-        }
-        try {
-            byte b[];
-            MessageDigest md = getMD();
-            md.reset();
-            md.update(encodeParam.getBytes(charset));
-            b = md.digest();
+  public static boolean checkRSASign(String content, String sign, String publicKey) {
+    return checkRSASign(content, sign, publicKey, CHARSET);
+  }
 
-            String sig = new String(Hex.encodeHex(b));
-            if (logger.isDebugEnabled()) {
-                logger.debug("calculateSig: sig=" + sig + "  , map=" + encodeParam);
-            }
-            return sig;
-        } catch (Exception e) {
-            logger.error("Encode MD5 Error", e);
-            return null;
-        }
-    }
+  public static String signMD5(String text, String key, String charset, boolean isAppend, boolean isUpperCase) {
+    String context = isAppend ? String.format("%s%s", text, key) : String.format("%s%s", key, text);
+    String sign = md5Hex(context, charset);
+    return isUpperCase ? StringUtils.upperCase(sign) : sign;
+  }
 
-    public static enum SigType {
-        MD5,
+  public static String signMD5(String text, String key) {
+    return signMD5(text, key, CHARSET, true, false);
+  }
 
-    }
+  public static String signMD5(String text, String key, boolean isUpperCase) {
+    return signMD5(text, key, CHARSET, true, isUpperCase);
+  }
 
-    /*public static String aliSdkSign(String content, String privateKey) {
-        try {
-            PKCS8EncodedKeySpec priPKCS8 = new PKCS8EncodedKeySpec(
-                    Base64.decode(privateKey));
-            KeyFactory keyf = KeyFactory.getInstance(ALGORITHM);
-            PrivateKey priKey = keyf.generatePrivate(priPKCS8);
+  public static boolean checkMD5Sign(String sign, String text, String key) {
+    return Objects.equals(sign, signMD5(text, key));
+  }
 
-            java.security.Signature signature = java.security.Signature
-                    .getInstance(SIGN_ALGORITHMS);
+  public static boolean checkMD5Sign(String sign, String text, String key, boolean isUpperCase) {
+    return Objects.equals(sign, signMD5(text, key, isUpperCase));
+  }
 
-            signature.initSign(priKey);
-            signature.update(content.getBytes(DEFAULT_CHARSET));
-
-            byte[] signed = signature.sign();
-
-            return Base64.encode(signed);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }*/
-
-    /*public static boolean rsaverify(String content, String sign,
-                                    String ali_public_key, String input_charset) throws Exception {
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        byte[] encodedKey = Base64.decode(ali_public_key);
-        PublicKey pubKey = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
-        java.security.Signature signature = java.security.Signature.getInstance("SHA1WithRSA");
-        signature.initVerify(pubKey);
-        signature.update(content.getBytes(input_charset));
-
-        boolean bverify = signature.verify(Base64.decode(sign));
-        return bverify;
-    }*/
-
-    /*public static Result md5sign(PMap<String, String> contextMap,
-                                 String md5securityKey, String charset) {
-        ResultMap result = ResultMap.build();
-        // 去掉map中的空值
-        PMap<String, String> signMap = new PMap<String, String>();
-        String value = "";
-        for (String Key : contextMap.keySet()) {
-            value = contextMap.get(Key);
-            if (value == null || value.equals("")
-                    || Key.equalsIgnoreCase("sign")
-                    || Key.equalsIgnoreCase("key")) {
-                continue;
-            }
-            signMap.put(Key, value);
-        }
-        // 组装签名报文
-        String sb = buildSignSource(signMap);
-        sb = sb + "&key=" + md5securityKey;
-        String signString;
-        try {
-            signString = DigestUtil.MD5Encode(sb, charset).toUpperCase();
-        } catch (Exception e) {
-            result.withError(ResultStatus.THIRD_SIGN_ERROR);
-            return result;
-        }
-        result.addItem("ChkValue", signString);
-        return result;
-    }*/
-
-    /*private static String buildSignSource(Map<String, String> contextMap) {
-        List keys = new ArrayList<String>(contextMap.keySet());
-        Collections.sort(keys);
-        String prestr = "";
-        for (int i = 0; i < keys.size(); i++) {
-            String key = keys.get(i).toString();
-            String value = contextMap.get(key);
-
-            if (i == keys.size() - 1) {// 拼接时，不包括最后一个&字符
-                prestr = prestr + key + "=" + value;
-            } else {
-                prestr = prestr + key + "=" + value + "&";
-            }
-        }
-        return prestr;
-    }*/
 }
--- thirdpay-service/src/main/java/com/sogou/pay/thirdpay/biz/utils/SecretKeyUtil.java
+++ thirdpay-service/src/main/java/com/sogou/pay/thirdpay/biz/utils/SecretKeyUtil.java
@@ -1,82 +1,49 @@
 package com.sogou.pay.thirdpay.biz.utils;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.KeyFactory;
-import java.security.Signature;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
-import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableList;
-import com.sogou.pay.common.utils.DigestUtil;
+import com.sogou.pay.common.utils.MapUtil;
+import com.sogou.pay.common.utils.SignUtil;
 import com.sogou.pay.common.types.PMap;
 
 public class SecretKeyUtil {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(SecretKeyUtil.class);
-
-  private static final String RSA = "RSA", SHA1WITHRSA = "SHA1WithRSA", CHARSET = "UTF-8";
-
-  public static String buildSignSource(Map<String, ?> map) {
-    if (MapUtils.isEmpty(map)) return "";
-    StringBuilder sb = new StringBuilder();
-    map.keySet().stream().sorted().forEach(k -> sb.append(k).append('=').append(map.get(k)).append('&'));
-    return sb.substring(0, sb.length() - 1);
-  }
-
-  public static String loadKeyFromFile(String keyFilePath) {
-    try {
-      return StringUtils.join(Files.readAllLines(Paths.get(keyFilePath)), null);
-    } catch (Exception e) {
-      LOGGER.error(String.format("read file error:path=%s", keyFilePath), e);
-      return null;
-    }
-  }
-
   public static String aliRSASign(PMap<String, ?> map, String privateKey) {
-    return aliRSASign(buildSignSource(filter(map, aliExcludedItems)), privateKey);
+    return aliRSASign(MapUtil.buildSignSource(MapUtil.filter(map, aliExcludedItems)), privateKey);
   }
 
   public static String aliRSASign(String content, String privateKey) {
-    return signSHA1WithRSA(content, privateKey);
+    return SignUtil.signSHA1WithRSA(content, privateKey);
   }
 
   public static boolean aliRSACheckSign(PMap<String, ?> contextMap, String sign, String publicKey) {
-    return aliRSACheckSign(buildSignSource(filter(contextMap, aliExcludedItems)), sign, publicKey);
+    return aliRSACheckSign(MapUtil.buildSignSource(MapUtil.filter(contextMap, aliExcludedItems)), sign, publicKey);
   }
 
   public static boolean aliRSACheckSign(String content, String sign, String publicKey) {
-    return checkRSASign(content, sign, publicKey);
+    return SignUtil.checkRSASign(content, sign, publicKey);
   }
 
   public static String aliMD5Sign(PMap<String, ?> contextMap, String key) {
-    return signMD5(buildSignSource(filter(contextMap, aliExcludedItems)), key);
+    return SignUtil.signMD5(MapUtil.buildSignSource(MapUtil.filter(contextMap, aliExcludedItems)), key);
   }
 
   public static boolean aliMD5CheckSign(PMap<String, ?> map, String key, String sign) {
-    return checkMD5Sign(sign, buildSignSource(filter(map, aliExcludedItems)), key);
+    return SignUtil.checkMD5Sign(sign, MapUtil.buildSignSource(MapUtil.filter(map, aliExcludedItems)), key);
   }
 
   public static String tenMD5Sign(PMap<String, ?> map, String key) {
-    return tenMD5Sign(buildSignSource(filter(map, tenExcludedItems)), key);
+    return tenMD5Sign(MapUtil.buildSignSource(MapUtil.filter(map, tenExcludedItems)), key);
   }
 
   public static String tenMD5Sign(String text, String key) {
-    return signMD5(text, getTenpayKey(key), true);
+    return SignUtil.signMD5(text, getTenpayKey(key), true);
   }
 
   public static boolean tenMD5CheckSign(PMap<String, ?> map, String key, String sign) {
-    return checkMD5Sign(sign, buildSignSource(filter(map, tenExcludedItems)), getTenpayKey(key), true);
+    return SignUtil.checkMD5Sign(sign, MapUtil.buildSignSource(MapUtil.filter(map, tenExcludedItems)),
+        getTenpayKey(key), true);
   }
 
   private static String getTenpayKey(String key) {
@@ -84,98 +51,22 @@ public class SecretKeyUtil {
   }
 
   public static String unionRSASign(PMap<String, ?> contextMap, String privateKey) {
-    return unionRSASign(buildSignSource(filter(contextMap, unionExcludedItems)), privateKey);
+    return unionRSASign(MapUtil.buildSignSource(MapUtil.filter(contextMap, unionExcludedItems)), privateKey);
   }
 
   public static String unionRSASign(String content, String privateKey) {
-    return signSHA1WithRSA(DigestUtil.SHAEncode(content, CHARSET), privateKey);
+    return SignUtil.signSHA1WithRSA(SignUtil.shaHex(content), privateKey);
   }
 
   public static boolean unionRSACheckSign(PMap<String, ?> map, String sign, String key) {
-    return unionRSACheckSign(buildSignSource(filter(map, unionExcludedItems)), sign, key);
+    return unionRSACheckSign(MapUtil.buildSignSource(MapUtil.filter(map, unionExcludedItems)), sign, key);
   }
 
   public static boolean unionRSACheckSign(String content, String sign, String key) {
-    return checkRSASign(DigestUtil.SHAEncode(content, CHARSET), sign, key);
-  }
-
-  private static String signSHA1WithRSA(String text, String privateKey) {
-    return signSHA1WithRSA(text, privateKey, CHARSET);
-  }
-
-  private static String signSHA1WithRSA(String text, String privateKey, String charset) {
-    try {
-      Signature signature = Signature.getInstance(SHA1WITHRSA);
-
-      signature.initSign(
-          KeyFactory.getInstance(RSA).generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKey))));
-      signature.update(text.getBytes(charset));
-
-      return Base64.getEncoder().encodeToString(signature.sign());
-    } catch (Exception e) {
-      LOGGER.error("signSHA1WithRSA error", e);
-      return null;
-    }
-  }
-
-  private static boolean checkRSASign(String content, String sign, String publicKey) {
-    return checkRSASign(content, sign, publicKey, CHARSET);
-  }
-
-  private static boolean checkRSASign(String content, String sign, String publicKey, String charset) {
-    try {
-      Signature signature = Signature.getInstance(SHA1WITHRSA);
-
-      signature.initVerify(
-          KeyFactory.getInstance(RSA).generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey))));
-      signature.update(content.getBytes(charset));
-
-      return signature.verify(Base64.getDecoder().decode(sign));
-    } catch (Exception e) {
-      LOGGER.error("checkRSASign error", e);
-      return false;
-    }
-  }
-
-  private static String signMD5(String text, String key) {
-    return signMD5(text, key, CHARSET, true, false);
-  }
-
-  private static String signMD5(String text, String key, boolean isUpperCase) {
-    return signMD5(text, key, CHARSET, true, isUpperCase);
-  }
-
-  private static String signMD5(String text, String key, String charset, boolean isAppend, boolean isUpperCase) {
-    String context = isAppend ? String.format("%s%s", text, key) : String.format("%s%s", key, text);
-    String sign = DigestUtil.MD5Encode(context, charset);
-    return sign == null || !isUpperCase ? sign : sign.toUpperCase();
-  }
-
-  private static boolean checkMD5Sign(String sign, String text, String key) {
-    return Objects.equals(sign, signMD5(text, key));
-  }
-
-  private static boolean checkMD5Sign(String sign, String text, String key, boolean isUpperCase) {
-    return Objects.equals(sign, signMD5(text, key, isUpperCase));
-  }
-
-  private static Map<String, ?> filter(Map<String, ?> map, List<String> excludedItems) {
-    return map.entrySet().stream()
-        .filter(e -> StringUtils.isNotEmpty(getString(e.getValue())) && !isOmitted(e.getKey(), excludedItems))
-        .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
-  }
-
-  private static boolean isOmitted(String item, List<String> excludedItems) {
-    for (String s : excludedItems)
-      if (s.equalsIgnoreCase(item)) return true;
-    return false;
+    return SignUtil.checkRSASign(SignUtil.shaHex(content), sign, key);
   }
 
   private static List<String> aliExcludedItems = ImmutableList.of("sign", "sign_type"),
       tenExcludedItems = ImmutableList.of("sign"), unionExcludedItems = ImmutableList.of("signature");
-
-  private static String getString(Object o) {
-    return o == null ? null : o.toString();
-  }
 
 }
